4219314c34311e988e65e3cf330b0d71083bb868
LM <wycalxy@outlook.com>
2026-04-12 18:09:38 +0800

    docs(collection): 添加 List 接口实现类的文档说明
    - 添加了 ArrayList 的特点说明：线程不安全、效率高、底层使用 Object[] 存储
    - 添加了 Vector 的特点说明：线程安全、效率低、底层使用 Object[] 存储
    - 添加了 LinkedList 的特点说明：底层使用双向链表存储
    - 说明了不同实现类在添加、删除、查找数据时的效率差异
    - 创建了新的文档类 _02 来整理 List 实现类的相关信息

---
+++ src/collection_map/collection/list/_02.java
@@ -0,0 +1,13 @@
+package collection_map.collection.list;
+
+/*
+ * list接口的实现类(动态数组)
+ *   ArrayList:List的主要实现类,线程不安全，效率高,底层使用Object[]存储
+ *             在添加数据，查找数据时，效率高;在删除，插入数据时，效率低
+ *   Vector:List古老实现类，线程安全，效率低,底层使用Object[]存储
+ *   LinkedList:底层使用双向链表存储
+ *               在添加，删除数据时，效率较高;添加，查找效率较低
+ * */
+
+public class _02 {
+}
