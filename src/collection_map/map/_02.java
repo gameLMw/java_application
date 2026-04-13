d6d25c3c66c9a72df59c8dd5a54a3030381c9ecb
LM <wycalxy@outlook.com>
2026-04-13 21:43:16 +0800

    docs(collection_map): 添加Map常用方法文档类
    - 新增Map常用方法的详细注释说明
    - 包含put、get、remove等核心操作方法介绍
    - 添加containsKey、containsValue查找方法说明
    - 补充clear、size、isEmpty等集合管理方法
    - 实现keySet方法返回所有键的集合功能

---
+++ src/collection_map/map/_02.java
@@ -0,0 +1,17 @@
+package collection_map.map;
+
+/*
+ * Map常用方法：
+ *       V put(K key, V value)：将指定的key-value添加到(当前)映射中，返回null
+ *       V get(Object key)：返回指定key对应的value
+ *       V remove(Object key)：删除指定key对应的数据，并返回value
+ *       boolean containsKey(Object key)：判断当前映射中是否包含指定的key
+ *       boolean containsValue(Object value)：判断当前映射中是否包含指定的value
+ *       void clear()：清空映射中的所有数据
+ *       int size()：返回映射中数据的个数
+ *       boolean isEmpty()：判断映射是否为空
+ *       Set<K> keySet()：返回所有key构成的Set集合
+ * */
+
+public class _02 {
+}
